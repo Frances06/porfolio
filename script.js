eac2af1d7c862c64a4733f7dc18b27891b74054b
Frances06 <b2205608@student.burycollege.ac.uk>
2026-07-13 20:08:17 +0100

    Made footer

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-/* NAV BAR */
+/* NAV BAR: Toggle hamburger menu */
 function navBar() {
     var x = document.getElementById("myTopNav");
     x.classList.toggle("responsive");
     x.classList.toggle("open");
 }
+
+/* FOOTER: Get current year and display it in the footer */
+window.addEventListener('load', function () {
+    function getCurrentYear() {
+        var year = new Date().getFullYear();
+        document.getElementById("currentYear").textContent = year;
+    }
+    getCurrentYear();
+});
